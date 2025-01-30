2ac9cc885dd526c26083b8c03e6d3ce30dd05b75
sergeemmanuelk <serge_em@outlook.fr>
2025-01-30 13:30:59 +0000

    Task 2: Add A New Auth Guard For Dentists

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,10 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'dentist' => [
+            'driver' => 'session',
+            'provider' => 'dentists',
+        ],
     ],
 
     /*
@@ -63,6 +67,10 @@ return [
         'users' => [
             'driver' => 'eloquent',
             'model' => env('AUTH_MODEL', App\Models\User::class),
+        ],
+        'dentists' => [
+            'driver' => 'eloquent',
+            'model' => env('AUTH_MODEL', App\Models\Dentist::class),
         ],
 
         // 'users' => [
